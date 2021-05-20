def9a2d8ef16514c346bdaf3e6d62fce390863e2
Douglas <capugougmor@gmail.com>
2021-05-20 12:24:44 -0300

    created project

---
+++ src/test/java/com/H/Cityapi/CityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.H.Cityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
